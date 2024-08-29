5d963faae7753d6ded40de97c316c52b606c215f
unknown <you@example.com>
2024-08-30 04:38:37 +0700

    03

--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
 const express = require('express')
-
+require('dotenv').config()
 const app = express()
+const cors = require('cors')
+const dbConnect = require('./config/dbconnect')
+const PORT = process.env.PORT || 7777
 
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+}))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.use('/', (req, res) =>
-    res.send('Server On')
-)
-app.listen(5000, () =>
-    console.log('::::::SERVER READY')
+dbConnect()
+
+app.listen(PORT, () =>
+    console.log('::::::SERVER READY ON PORT: ' + PORT)
 )
