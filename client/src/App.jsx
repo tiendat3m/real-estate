2969ab53365f0a672749431a0fee49380f6ba7e7
unknown <phantiendat14012002@gmail.com>
2024-11-15 02:27:17 +0700

    rest 06

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+
+const App = () => {
+  return (
+    <div className='flex items-center justify-center h-screen w-screen'>
+      <Button variant='outline'>
+        CLick me
+      </Button>
+    </div>
+  )
+}
+
+export default App
---
+++ client/vite.config.js
@@ -0,0 +1,14 @@
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+
+const __dirname = path.resolve(path.dirname(""))
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
